27202938786b7d797a73b16fdc6f39eca670fb51
nohannah <nohannah508@gmail.com>
2025-01-02 23:57:03 +0600

    basics syntax

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo "Hello world";?></title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p><?php echo "This is my favorite page";?></p>
+
+</body>
+</html>
+
+<?php echo 9*8;?>
