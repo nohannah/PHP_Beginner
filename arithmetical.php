f7831de1d23fd4eb754fd322b6be1fdd017d2faf
nohannah <nohannah508@gmail.com>
2025-01-18 21:29:34 +0600

    comparison

--- arithmetical.php
+++ arithmetical.php
@@ -1,22 +1,26 @@
 <?php 
-// echo 8+8.9;
-// echo 9%4; 
+// // echo 8+8.9;
+// // echo 9%4; 
 
-$num=99;
-//$num +=10;
-$num -=9;
-//$num = $num+9; 
-echo $num;
-//comparison operator
-$x =10; 
-$y="2";
+// $num=99;
+// //$num +=10;
+// $num -=9;
+// //$num = $num+9; 
+// echo $num;
+// //comparison operator
+// $x =10; 
+// $y="2";
 
-echo gettype ($y);
-var_dump($x=== $y);
+// echo gettype ($y);
+// var_dump($x=== $y);
 
-// increment and decrement 
+// // increment and decrement 
 
-$sum=9; 
-echo ++$sum;
-echo --$sum;
+// $sum=9; 
+// echo ++$sum;
+// echo --$sum;
+//comparison operator 
+$x=10;
+$y=2;
+var_dump($x==$y);
 ?>
